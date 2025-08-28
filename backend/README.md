6fe03dcc2d6c10629446f331093aad8c405a7cfb
Kisan-jena <kisan.k.k.jena.01@gmail.com>
2025-08-29 01:51:19 +0530

    done with basic setup

--- backend/README.md
+++ backend/README.md
@@ -1,15 +1,2 @@
-# backend
-
-To install dependencies:
-
-```bash
-bun install
-```
-
-To run:
-
-```bash
-bun run server.ts
-```
-
-This project was created using `bun init` in bun v1.2.21. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
+Backend-Schema
+![alt text](image.png)
--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from 'cors'
+
+const app=express()
+const port=3000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({ message: "Server is running!" })
+})
+
+app.listen(
+    port,
+    ()=>console.log(`server started on http://localhost:${port}`)
+)
